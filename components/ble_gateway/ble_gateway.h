d1e222b14050d293e780f109f46c4470efa778e6
IanThewlis <ian.thewlis@iancrt.co.uk>
2025-07-16 14:05:15 +0100

    Update ble_gateway.h

--- components/ble_gateway/ble_gateway.h
+++ components/ble_gateway/ble_gateway.h
@@ -1,4 +1,4 @@
-#pragma once
+phome#pragma once
 
 #ifdef USE_ESP32
 
@@ -19,29 +19,29 @@ namespace ble_gateway {
 class BLEGateway : public Component, public esp32_ble_tracker::ESPBTDeviceListener {
  public:
   float get_setup_priority() const override { return setup_priority::DATA; }
-  void add_callback(std::function<void(const esp32_ble_tracker::ESPBTDevice &, std::string)> &&callback) { this->callback_.add(std::move(callback)); }
+  void add_callback(std::function<void(const esp32_ble_tracker::ESPBTDevice &, esphome::string)> &&callback) { this->callback_.add(std::move(callback)); }
 
 #if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 6, 0)
-  static std::string scan_result_to_hci_packet_hex(const esp32_ble::BLEScanResult &scan_result);
+  static esphome::string scan_result_to_hci_packet_hex(const esp32_ble::BLEScanResult &scan_result);
 #else
-  static std::string scan_result_to_hci_packet_hex(const esp_ble_gap_cb_param_t::ble_scan_result_evt_param &scan_result);
+  static esphome::string scan_result_to_hci_packet_hex(const esp_ble_gap_cb_param_t::ble_scan_result_evt_param &scan_result);
 #endif
   void dump_config() override;
   bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override;
   bool get_discovery() const { return this->discovery_; }
   void set_discovery(bool discovery) { this->discovery_ = discovery; }
   void add_device(uint64_t device);
-  void set_devices(std::string devices);
+  void set_devices(esphome::string devices);
  protected:
   bool discovery_{false};
   std::vector<uint64_t> devices_{};
-  CallbackManager<void(const esp32_ble_tracker::ESPBTDevice &, std::string)> callback_{};
+  CallbackManager<void(const esp32_ble_tracker::ESPBTDevice &, esphome::string)> callback_{};
 };
 
-class BLEGatewayBLEAdvertiseTrigger : public Trigger<const esp32_ble_tracker::ESPBTDevice &, std::string> {
+class BLEGatewayBLEAdvertiseTrigger : public Trigger<const esp32_ble_tracker::ESPBTDevice &, esphome::string> {
  public:
   explicit BLEGatewayBLEAdvertiseTrigger(BLEGateway *parent) {
-   parent->add_callback([this](const esp32_ble_tracker::ESPBTDevice &value, std::string packet) {
+   parent->add_callback([this](const esp32_ble_tracker::ESPBTDevice &value, esphome::string packet) {
     this->trigger(value, packet); });
   }
 };
